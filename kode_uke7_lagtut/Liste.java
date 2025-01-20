1bdd087315025064026df3d2b3eaaf71b3b4edfe
trulsde <trulsde@ifi.uio.no>
2025-01-20 17:09:42 +0100

    Delete kode_uke7_lagtut directory

--- kode_uke7_lagtut/Liste.java
+++
@@ -1,8 +0,0 @@
-
-interface Liste<T> extends Iterable<T>{
-	int size();
-	void add(T x);
-	void set(int pos, T x);
-	T get(int pos);
-	T remove(int pos);
-}
--- kode_uke7_lagtut/UlovligListeIndeks.java
+++
@@ -1,5 +0,0 @@
-class UlovligListeIndeks extends RuntimeException {
-    public UlovligListeIndeks(int pos, int max) {
-        super("Listeindeks " + pos + " ikke i intervallet 0-" + max);
-    }
-}
